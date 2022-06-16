636dc97b8451b4de8ab8a2c1cace12e7a49c36bd
Imangali017 <imangalibolatbek@gmail.com>
2022-06-16 13:42:31 +0600

    initial commit

--- src/main/java/kz/springboot/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/kz/springboot/springbootdemo/SpringbootdemoApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootdemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootdemoApplication.class, args);
+	public static void main(String args [])
+	{
+		System.out.println("Hello World");
 	}
 
 }
