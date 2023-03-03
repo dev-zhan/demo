308d4ca20611dc7f2ddf3d4ee54fa78c79e55fde
rakhimzhanrashidov <rakhimzhanrashidov@gmail.com>
2023-03-03 11:33:35 +0600

    Revert "initial commit"
    This reverts commit 636dc97b8451b4de8ab8a2c1cace12e7a49c36bd.

--- src/main/java/kz/springboot/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/kz/springboot/springbootdemo/SpringbootdemoApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootdemoApplication {
 
-	public static void main(String args [])
-	{
-		System.out.println("Hello World");
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoApplication.class, args);
 	}
 
 }
